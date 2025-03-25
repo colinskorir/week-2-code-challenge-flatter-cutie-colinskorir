45ee59f567c2b00b9d51a41f3ce9f0d0d79e8514
colinskorir <korircoll20@gmail.com>
2025-03-25 09:09:06 +0300

    index.js files added

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@ const characterBar = document.getElementById("character-bar");
 const detailedInfo = document.getElementById("detailed-info");
 const votesForm = document.getElementById("votes-form");
 
+function fetchCharacters() {
+  fetch(`${BASE_URL}`)
+    .then((response) => response.json())
+    .then((characters) => {
+      characters.forEach((character) => {
+        const span = document.createElement("span");
+        span.textContent = character.name;
+        span.addEventListener("click", () => displayCharacterDetails(character));
+        characterBar.appendChild(span);
+      });
+    });
+}
+
+
 fetchCharacters();
