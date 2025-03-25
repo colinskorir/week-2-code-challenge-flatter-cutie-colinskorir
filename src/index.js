024dea04b90beda6d94dce7c4f7ebdc6d1b47b61
colinskorir <korircoll20@gmail.com>
2025-03-25 09:07:03 +0300

    files added

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const BASE_URL = "http://localhost:3000/characters";
+
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const votesForm = document.getElementById("votes-form");
+
